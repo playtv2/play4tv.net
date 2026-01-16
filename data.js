3893a1577cfe9aba16b0bfa71222a7d0022d3139
tv <nitaybensitrit123@gmail.com>
2026-01-16 20:26:23 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,47 +1,22 @@
 const seriesData = [
     {
         id: "al-atzmi",
-        type: "series",
-        category: "דרמה",
         title: "אל עצמי",
-        description: "סיפורו המרגש של ציון, ילד המנסה למצוא את מקומו בעולם ועובר לגור עם משפחה אומנת בחיפה.",
-        image: "https://www.yes.co.il/media/ux0f43ln/vp004414285.jpg?quality=80", // פוסטר הסדרה
+        description: "סיפורו של ציון...",
+        image: "hhttps://www.yes.co.il/media/ux0f43ln/vp004414285.jpg?quality=80",
         episodes: [
-            { name: "פרק 1", desc: "תקציר הפרק", driveId: "1-TJDR-bjqha_x-DHndKd-uvGIuQU3Lz0" },
-            { name: "פרק 2", desc: "תקציר הפרק", driveId: "10Flj4VBaJ4ANqWfy8Lt0BRTPwsrITPHZ" },
-            { name: "פרק 3", desc: "תקציר הפרק", driveId: "16Uy_zP90R2V8wI8f5PcQYer3kmfZon7M" },
-            { name: "פרק 4", desc: "תקציר הפרק", driveId: "1BT6x7j_59ptqyVyoIbBaZbzeHwrBAq0b" },
-            { name: "פרק 5", desc: "תקציר הפרק", driveId: "1DXLtQqtwL44GsS3Gr70sJDuIxjjhrQ0b" },
-            { name: "פרק 6", desc: "תקציר הפרק", driveId: "1Kmwm0nDrLFPBcndl9vwr1WNhg_Pe1vxA" },
-            { name: "פרק 7", desc: "תקציר הפרק", driveId: "1QACnT01BTBwxeQFgEtgO0HJ2jKMuN94K" },
-            { name: "פרק 8", desc: "תקציר הפרק", driveId: "1Rg-WeOCT-Daba-IzGy919JHTnl2BivEy" },
-            { name: "פרק 9", desc: "תקציר הפרק", driveId: "1SGOImupPoUKNaR89lRXhaRLWhteyLZwa" },
-            { name: "פרק 10", desc: "תקציר הפרק", driveId: "1ThXyiGhyJ6-FQkLHh_ERzePmxp1LPm8V" },
-            { name: "פרק 11", desc: "תקציר הפרק", driveId: "1g6-62xZO3gXPQniuaqzMfztolZkuBGlx" },
-            { name: "פרק 12", desc: "תקציר הפרק", driveId: "1nWL_FVXbI1gCi5_rJPfdVmstn8bTJr4p" },
-            { name: "פרק 13", desc: "תקציר הפרק", driveId: "1pCWPvXCeWlnEwjB56-qbr2OrpZZaZp5I" },
-            { name: "פרק 14", desc: "תקציר הפרק", driveId: "1tkKSN3c_axX-hXTUPIC3iHAFlO9j_rA3" },
-            { name: "פרק 15", desc: "תקציר הפרק", driveId: "1vljlNJ-49LiQgdGFS_Yt6YPptlhxt5q_" },
-            { name: "פרק 16", desc: "תקציר הפרק", driveId: "1w9AXhr23xKRme5GxiWgFUkU5qVqAbLX3" }
-        ]
-
-        const seriesData = [
-    {
-        id: "kupa-rashit",
-        title: "קופה ראשית - העונה החדשה",
-        description: "סדרת הקאלט המוקומנטרית המתקתחת בסניף של רשת סופרים ביבנה. הדמויות נשלפו היישר מהסופר הישראלי...",
-        image: "https://link-to-kupa-rashit-banner.jpg", // התמונה הגדולה מלמעלה
-        category: "קומדיה",
-        episodes: [
-            { 
+            {
                 number: "1",
-                name: "פרק 1", 
-                desc: "פרק פתיחת העונה! שנה חדשה נפתחת בסופר. שירה מבקשת מאנטולי לקחת את המתנה שלו לחג אבל העניינים מסתבכים...", 
-                image: "https://link-to-ep1-image.jpg", // התמונה של הפרק עצמו
-                driveId: "ID_של_הפרק_מהדרייב" 
+                desc: "תקציר פרק 1",
+                image: "https://i.ibb.co/pWv6GZ7/ep1.jpg",
+                driveId: "1-TJDR-bjqha_x-DHndKd-uvGIuQU3Lz0"
+            },
+            {
+                number: "2",
+                desc: "תקציר פרק 2",
+                image: "https://i.ibb.co/pWv6GZ7/ep2.jpg",
+                driveId: "10Flj4VBaJ4ANqWfy8Lt0BRTPwsrITPHZ"
             }
         ]
-    }
-];
     }
 ];
