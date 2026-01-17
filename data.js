2dac202f376ada1760b0f0c48b5fd77e11bffa8b
tv <nitaybensitrit123@gmail.com>
2026-01-17 12:43:08 +0200

    Update data.js

--- data.js
+++ data.js
@@ -42,7 +42,7 @@ const seriesData = [
     {
     id: "zootopia2",  // מזהה ייחודי באנגלית (שלא יחזור על עצמו)
     type: "movie",             // סוג התוכן
-    category: "ילדים", "סרטים", 
+    category: "ילדים" 
     title: "זוטרופוליס 2",
     description: "כאן יופיע תקציר הסרט.",
     image: "https://m.media-amazon.com/images/M/MV5BYjg1Mjc3MjQtMTZjNy00YWVlLWFhMWEtMWI3ZTgxYjJmNmRlXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", // תמונה לאורך (Poster)
